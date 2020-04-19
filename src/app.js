d93bc6bbe131a99d281bfafa7b2d0a124fec3f19
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-04-19 18:21:57 +0200

    feat: adding db connection

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const HTTP_CODE = require('http-status-codes');
 const cors = require('cors');
 
+const router = require('./router');
+const { API_VERSION } = require('./constants');
+const { db } = require('./helpers');
+
 const app = express();
+
+db.connect();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/status', (req, res) => res.sendStatus(HTTP_CODE.OK));
+app.get('/status', (_, res) => res.sendStatus(HTTP_CODE.OK));
+app.use(`/api/${API_VERSION}`, router);
 
 module.exports = app;
