36e5e17b41253c8ed01aff264045bb4e266ac57c
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-04-19 18:26:00 +0200

    feat: auth middleware

---
+++ src/middlewares/auth-guard.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const HTTP_CODE = require('http-status-codes');
+
+const authGuard = (req, res, next) => {
+	let token;
+	const bearer = req.headers.authorization;
+
+	if (!bearer) return res.set(HTTP_CODE.UNAUTHORIZED).json({ msg: 'Access denied.' });
+
+	const parts = bearer.split(' ');
+
+	if (parts.length === 2 && parts[0] === 'Bearer') token = parts[1];
+
+	try {
+		const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+		req.user = verified;
+		next();
+	} catch {
+		return res.set(HTTP_CODE.BAD_REQUEST).json({ msg: 'Invalid Token.' });
+	}
+};
+
+module.exports = authGuard;
