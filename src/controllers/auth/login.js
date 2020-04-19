3f6fdda07d75c2534ab61f87d9f5d8d7224a2dee
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-04-19 18:27:25 +0200

    feat: adding auth routes

---
+++ src/controllers/auth/login.js
@@ -0,0 +1,28 @@
+const HTTP_CODE = require('http-status-codes');
+const { User, joiLoginSchema } = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const login = async (req, res) => {
+	try {
+		const { error } = joiLoginSchema.validate(req.body, { abortEarly: false });
+
+		if (error) return res.set(HTTP_CODE.BAD_REQUEST).json({ error });
+
+		const user = await User.findOne({ email: req.body.email });
+
+		if (!user) return res.set(HTTP_CODE.BAD_REQUEST).json({ message: 'Email or password is wrong.' });
+
+		const validPass = await bcrypt.compare(req.body.password, user.password);
+
+		if (!validPass) return res.set(HTTP_CODE.BAD_REQUEST).json({ message: 'Email or password is wrong.' });
+
+		const token = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN_SECRET);
+
+		return res.set(HTTP_CODE.OK).json({ auth: token });
+	} catch {
+		return res.set(HTTP_CODE.INTERNAL_SERVER_ERROR);
+	}
+};
+
+module.exports = login;
---
+++ src/controllers/auth/register.js
@@ -0,0 +1,38 @@
+const HTTP_CODE = require('http-status-codes');
+const { User, joiUserSchema } = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+	try {
+		const { error } = joiUserSchema.validate(req.body, { abortEarly: false });
+
+		if (error) return res.set(HTTP_CODE.BAD_REQUEST).json({ error });
+
+		const userExists = await User.findOne({ email: req.body.email });
+
+		if (userExists) return res.set(HTTP_CODE.BAD_REQUEST).json({ message: 'Email already used.' });
+
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+		const user = new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: hashPassword,
+		});
+
+		try {
+			await user.save();
+			const token = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN_SECRET);
+
+			return res.set(HTTP_CODE.CREATED).json({ auth: token });
+		} catch {
+			return res.sendStatus(HTTP_CODE.INTERNAL_SERVER_ERROR);
+		}
+	} catch {
+		return res.sendStatus(HTTP_CODE.INTERNAL_SERVER_ERROR);
+	}
+};
+
+module.exports = register;
