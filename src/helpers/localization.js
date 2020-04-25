776741f0b40477baf10f0f40ce196ce17442fc08
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-04-25 22:01:09 +0200

    feat: adding coordinates

--- src/helpers/localization.js
+++ src/helpers/localization.js
@@ -1,5 +1,10 @@
-const computeCities = async cities =>
-  cities.map(city => ({ name: city.nom, zip: city.codesPostaux, department: city.departement }));
+const computeCities = async collection =>
+  collection.features.map(feature => ({
+    name: feature.properties.nom,
+    zip: feature.properties.codesPostaux,
+    department: feature.properties.departement,
+    coordinates: feature.geometry.coordinates
+  }));
 
 module.exports = {
   computeCities
