bce0570b09c2e8e33724e4cf2f7cbb17f46ea97f
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-04-25 22:26:27 +0200

    feat: removing department

--- src/helpers/localization.js
+++ src/helpers/localization.js
@@ -1,9 +1,8 @@
 const computeCities = async collection =>
-  collection.features.map(feature => ({
+  collection.features.slice(0, 20).map(feature => ({
     name: feature.properties.nom,
     zip: feature.properties.codesPostaux,
-    department: feature.properties.departement,
-    coordinates: feature.geometry.coordinates
+    coordinates: feature.geometry.coordinates.reverse()
   }));
 
 module.exports = {
